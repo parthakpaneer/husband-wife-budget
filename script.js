63122f652931c7c9d6000fd05eb54b2c960182ed
parthakpaneer <pandeysarthakjee@gmail.com>
2025-06-23 21:13:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -116,3 +116,16 @@ function deleteExpense(id) {
   expenses = expenses.filter(exp => exp.id !== id);
   updateDisplay();
 }
+
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "your-project-id.firebaseapp.com",
+  databaseURL: "https://your-project-id-default-rtdb.firebaseio.com",
+  projectId: "your-project-id",
+  storageBucket: "your-project-id.appspot.com",
+  messagingSenderId: "123456789",
+  appId: "1:123456789:web:abcdef"
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
